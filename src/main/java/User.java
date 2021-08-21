3d8e2ea2b242ebce47bc96ff9eded80c226d7ee5
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-21 17:28:02 +0530

    Add

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -12,4 +12,12 @@ public class User {
 	public boolean checkEmail(String s) {
 		return s.matches("[A-Za-z][A-Za-z0-9.-_]*[@][a-z]+[.][a-z]{2,3}.[a-z]{2,3}");
 	}
+	
+	public boolean checkPhone(String s) {
+		return s.matches("[0-9]{2,3}[ ][0-9]{10}");
+	}
+	
+	public boolean checkPassword(String s) {
+		return s.matches("(?=.*[a-z])(?=.*[A-Z]).{8,}");
+	}
 }
