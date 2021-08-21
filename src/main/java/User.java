04376bbcf90147dffd322f28f00e0d4f3cffeecd
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-21 17:24:57 +0530

    Add

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,6 +1,11 @@
 
 public class User {
+	
 	public boolean checkFirstName(String s) {
+		return s.matches("[A-Z][a-z]{2,}");
+	}
+
+	public boolean checkLastName(String s) {
 		return s.matches("[A-Z][a-z]{2,}");
 	}
 }
