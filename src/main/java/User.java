54c20fc221dbc8c0115a5bb48804787e0d543380
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-08-21 17:25:46 +0530

    Add

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -8,4 +8,8 @@ public class User {
 	public boolean checkLastName(String s) {
 		return s.matches("[A-Z][a-z]{2,}");
 	}
+	
+	public boolean checkEmail(String s) {
+		return s.matches("[A-Za-z][A-Za-z0-9.-_]*[@][a-z]+[.][a-z]{2,3}.[a-z]{2,3}");
+	}
 }
